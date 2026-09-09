286d0aa60abcd93d1d8053ea0c0c3617ba199a4b
Malik05255 <mmbbrr99@gmail.com>
2026-09-10 01:43:05 +0300

    Build app against Android API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.malik.ztesmartmanager"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.malik.ztesmartmanager"
