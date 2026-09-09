a5f292a5604a44e4c466e19a0a7f2b19d7f77421
Malik05255 <mmbbrr99@gmail.com>
2026-09-10 01:38:08 +0300

    Use stable Android API 36 for first build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.malik.ztesmartmanager"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.malik.ztesmartmanager"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
