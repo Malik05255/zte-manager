7523e74c82cb5df5d0155a2f5dac3ae81faf3db8
Malik05255 <mmbbrr99@gmail.com>
2026-09-10 01:39:39 +0300

    Add explicit Android coroutines dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
     implementation(libs.androidx.core)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.kotlinx.coroutines.android)
 
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
