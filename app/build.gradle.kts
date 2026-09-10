158159dd10d95956cf8e7f64dd6e5830958a04ed
Malik05255 <mmbbrr99@gmail.com>
2026-09-10 16:35:38 +0300

    Bump Arabic RTL release to 0.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.malik.ztesmartmanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 29
-        versionName = "0.5.1"
+        versionCode = 30
+        versionName = "0.5.2"
     }
 
     buildFeatures {
