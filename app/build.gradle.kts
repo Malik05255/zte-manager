21b87e8fe57793c9a2341220108b38e7afbf2b54
Malik05255 <mmbbrr99@gmail.com>
2026-09-10 10:07:42 +0300

    Add JUnit dependency for truth-first parser tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,5 +40,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
+    testImplementation("junit:junit:4.13.2")
+
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
