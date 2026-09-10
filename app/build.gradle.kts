9e9046d36f72683630be73a374473eb2bf0c4420
Malik05255 <mmbbrr99@gmail.com>
2026-09-10 10:23:13 +0300

    Bump reference dashboard build to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.malik.ztesmartmanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.2.0"
+        versionCode = 8
+        versionName = "0.2.1"
     }
 
     buildFeatures {
