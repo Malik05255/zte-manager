5483dd23772b44e35977fe2a2adfca9b2727c6d4
Malik05255 <mmbbrr99@gmail.com>
2026-09-10 13:35:39 +0300

    Bump responsive release to 0.3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.malik.ztesmartmanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "0.3.3"
+        versionCode = 21
+        versionName = "0.3.4"
     }
 
     buildFeatures {
