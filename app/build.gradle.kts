e0533215eb647f02ce9c6d8dcf85e650cd82e281
Malik05255 <mmbbrr99@gmail.com>
2026-09-10 13:21:10 +0300

    Bump verified signal history release to 0.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.malik.ztesmartmanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.3.2"
+        versionCode = 20
+        versionName = "0.3.3"
     }
 
     buildFeatures {
