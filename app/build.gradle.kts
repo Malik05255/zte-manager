2b84889150691ecb3c914717a654ef3b22ad5c7f
Malik05255 <mmbbrr99@gmail.com>
2026-09-10 13:10:22 +0300

    Bump traffic telemetry release to 0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.malik.ztesmartmanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 18
-        versionName = "0.3.1"
+        versionCode = 19
+        versionName = "0.3.2"
     }
 
     buildFeatures {
