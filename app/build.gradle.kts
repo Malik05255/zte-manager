b45c7726edce3ee0b548ac0f2e208b5f34215b11
Malik05255 <mmbbrr99@gmail.com>
2026-09-10 11:51:09 +0300

    Bump app to 0.2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.malik.ztesmartmanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.2.5"
+        versionCode = 13
+        versionName = "0.2.6"
     }
 
     buildFeatures {
