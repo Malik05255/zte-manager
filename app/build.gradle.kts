20fff43a514b1b9a14ce835b7f6bc40597950cfb
Malik05255 <mmbbrr99@gmail.com>
2026-09-10 14:16:14 +0300

    Bump app to 0.3.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.malik.ztesmartmanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "0.3.6"
+        versionCode = 24
+        versionName = "0.3.7"
     }
 
     buildFeatures {
