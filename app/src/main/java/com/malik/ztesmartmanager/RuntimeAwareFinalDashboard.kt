6edebf90e491e8548b41f6ddecec09ce14134322
Malik05255 <mmbbrr99@gmail.com>
2026-09-11 21:17:16 +0300

    Route production shell to Nova UI

--- app/src/main/java/com/malik/ztesmartmanager/RuntimeAwareFinalDashboard.kt
+++ app/src/main/java/com/malik/ztesmartmanager/RuntimeAwareFinalDashboard.kt
@@ -133,7 +133,7 @@ fun RuntimeAwareFinalDashboard(
         supportsAntennaControl = runtime?.antennaControl?.canAttemptWrite == true
     )
 
-    ImmersiveDashboard(
+    NovaDashboard(
         snapshot = snapshot,
         capabilities = effectiveCapabilities,
         runtime = runtime,
