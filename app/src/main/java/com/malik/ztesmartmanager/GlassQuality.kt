71a83d74668e1a89312a201ff47efd760d91207e
Malik05255 <mmbbrr99@gmail.com>
2026-09-12 02:27:25 +0300

    Delete every old dashboard implementation

--- app/src/main/java/com/malik/ztesmartmanager/GlassQuality.kt
+++
@@ -1,6 +0,0 @@
-package com.malik.ztesmartmanager
-
-import com.malik.ztesmartmanager.core.model.RouterSnapshot
-import com.malik.ztesmartmanager.core.smart.NetworkQualityEngine
-
-internal fun qualityFor(snapshot: RouterSnapshot) = NetworkQualityEngine().score(snapshot)
