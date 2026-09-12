f62ac566312e5ef5308d80579c502a3f9e845036
Malik05255 <mmbbrr99@gmail.com>
2026-09-12 03:44:34 +0300

    Delete superseded UI compatibility shim

--- app/src/main/java/com/malik/ztesmartmanager/ZteSaveableCompat.kt
+++
@@ -1,8 +0,0 @@
-package com.malik.ztesmartmanager
-
-import androidx.compose.runtime.Composable
-
-/** Keeps redesigned screens concise while delegating state persistence to Compose. */
-@Composable
-internal fun <T : Any> rememberSaveable(init: () -> T): T =
-    androidx.compose.runtime.saveable.rememberSaveable(init = init)
